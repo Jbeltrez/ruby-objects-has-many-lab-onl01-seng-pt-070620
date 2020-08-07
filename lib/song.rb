ce39bc002cf49b482c5c77ffdf64f8fa6bd70910
john beltrez <beltrezjohn@gmail.com>
2020-08-07 01:10:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,26 +4,15 @@ class Song
   def initialize(name)
     @name = name
     @@all << self
-    
-    
   end
   def self.all 
     @@all
   end
   def artist_name 
-     
     if self.artist
       artist.name
     else 
       nil 
     end
-    
   end 
-  
-
-  
-    
-  
-  
-  
 end
